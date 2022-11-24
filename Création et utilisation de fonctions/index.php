dc3b54055b9d70e0a5928a3c34295e7df4533ef8
Gerimkhanov Yasin <200460@CCM-Asty-Moulin.local>
2022-11-24 13:49:47 +0100

    ajout de code pour compter le nombre de lettre

--- Création et utilisation de fonctions/index.php
+++ Création et utilisation de fonctions/index.php
@@ -13,17 +13,28 @@
 function factorielle($nombreDepart)
 {
     $resultatFact = 1;
-    for ($i = 1; $i < $nombreDepart;$i++) {
+    for ($i = 1; $i <= $nombreDepart;$i++) {
         $resultatFact = $resultatFact * $i ;
     }
     return $resultatFact; 
+}
+
+function nombreLettre($phrase, $lettre) 
+{
+    $nombreCompteur = 0;
+    for ($i = 0; $i <strlen($phrase) ;$i++) {
+        if ($phrase[$i] == $lettre){
+            $nombreCompteur++;
+        }
+    }
+    return $nombreCompteur;
 }
 
 ?>
 
     <!-- partie affichage -->
     <p>la factorille de 7 vaut <?= factorielle(7) ?></p>
-
+    <p>il y a  <?= nombreLettre("bonjour tous le monde" , "o")   ?> "o" dans bonjour tous le monde  </p>
 
 
 
